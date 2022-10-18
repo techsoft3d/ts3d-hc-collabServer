c6f0c712b42c65aaabfb5a76d92cd33a17496766
Guido <eric5544@gmail.com>
2022-10-18 06:56:26 -0700

    project structure simplified

--- server/app.js
+++ server/app.js
@@ -1,52 +1,188 @@
-const fs = require('fs');
-const path = require('path');
+const { Server } = require("socket.io");
+var io;
 
-const http = require('http');
+let userinfo = [];
+let rooms = [];
+var totalusers = 0;
 
-const express = require('express');
-const cors = require('cors');
-
-const config = require('config');
-
-const collab = require('./collab');
+var allowUserRooms = true;
 
 
-exports.start = async function (server, config) {
+exports.createRoom = (roomname, password) => {
 
-  return collab.start(server, config);
-   
+  rooms[roomname] = { lockedMaster: "", password: password, users: 0, userroom: false };
+
+};
+
+exports.deleteRoom = (roomname) => {
+
+  if (rooms[roomname] != undefined) {
+    delete rooms[roomname];
+  }
+
 };
 
 
+exports.start = (httpServer, config) => {
 
-exports.createRoom = async function (roomname, password) {
+  io = new Server(httpServer, {
+    cors: {
+      origin: "*"
+    }
+  });
 
-  collab.createRoom(roomname, password);
-   
+  if (config && config.allowUserRooms != undefined) {
+    allowUserRooms = config.allowUserRooms;
+  }
+
+
+  io.on('connection', (socket) => {
+    totalusers++;
+    let userId = socket.id;
+
+    socket.on('disconnect', () => {
+      totalusers--;
+      console.log('User disconnected. Total users:' + totalusers);
+      if (userinfo[socket.id] != undefined) {
+        sendUserList(socket);
+        if (rooms[userinfo[socket.id].roomname].lockedMaster == socket.id) {
+          rooms[userinfo[socket.id].roomname].lockedMaster = "";
+        }
+
+        rooms[userinfo[socket.id].roomname].users--;
+        if (rooms[userinfo[socket.id].roomname].users == 0 && rooms[userinfo[socket.id].roomname].userroom) {
+          delete rooms[userinfo[socket.id].roomname];
+        }
+      }
+
+      sendToRoom(socket, 'disconnected', socket.id);
+      delete userinfo[socket.id];
+    });
+
+    socket.on('joinroom', (msg) => {
+
+
+
+      let joininfo = JSON.parse(msg);
+      console.log('a user connected:' + joininfo.username + " to " + joininfo.roomname + " at " + new Date() + ' Total users:' + totalusers);
+
+
+      if (rooms[joininfo.roomname] == undefined && !allowUserRooms) {
+        return;
+      }
+
+      if (rooms[joininfo.roomname] != undefined && rooms[joininfo.roomname].password && (rooms[joininfo.roomname].password != joininfo.password)) {
+        return;
+      }
+
+      userinfo[socket.id] = joininfo;
+
+
+      if (rooms[userinfo[socket.id].roomname] == undefined) {
+        rooms[userinfo[socket.id].roomname] = { lockedMaster: "", password: joininfo.password,users:0, userroom: true };
+      }      
+      rooms[userinfo[socket.id].roomname].users++;
+
+      socket.join(joininfo.roomname);
+
+      sendUserList(socket);
+      if (rooms[userinfo[socket.id].roomname].lockedMaster != "") {
+        sendToUser(socket.id, "lockSession", rooms[userinfo[socket.id].roomname].lockedMaster);
+      }
+      sendInitialState(socket, joininfo.roomname);
+
+    });
+
+    socket.on('hcmessage', (msg) => {
+      sendToRoom(socket, 'hcmessage', msg);
+    });
+
+    socket.on('initialState', (msg) => {
+      let sinfo = JSON.parse(msg);
+      sendToUser(sinfo.recepient, "initialState", JSON.stringify(sinfo.state));
+    });
+
+
+
+    socket.on('chatmessage', (msg) => {
+      sendToRoom(socket, 'chatmessage', msg);
+    });
+
+    socket.on('run', (msg) => {
+      sendToRoom(socket, 'run', msg);
+    });
+
+
+    socket.on('lockSession', (msg) => {
+      rooms[userinfo[socket.id].roomname].lockedMaster = socket.id;
+      sendToRoom(socket, 'lockSession', socket.id);
+    });
+
+    socket.on('unlockSession', (msg) => {
+      rooms[userinfo[socket.id].roomname].lockedMaster = "";
+      sendToRoom(socket, 'unlockSession', msg);
+    });
+
+
+  });
+  return io;
 };
 
 
-exports.deleteRoom = async function (roomname) {
+function getUserList(socket) {
+  let roomname = userinfo[socket.id].roomname;
+  let userset = io.sockets.adapter.rooms.get(roomname);
 
-  collab.deleteRoom(roomname);
-   
-};
+  let userlist = [];
+  if (userset != undefined) {
+    for (let item of userset) {
+      userlist.push({ username: userinfo[item].username, id: item });
+    }
+  }
+
+  return userlist;
+
+}
+
+
+function sendUserList(socket) {
+  let users = getUserList(socket);
+  sendToRoomIncludingMe(socket, "userlist", JSON.stringify({ user: socket.id, roomusers: users }));
+}
+
+function sendToUser(user, msgtype, msg) {
+  io.to(user).emit(msgtype, msg);
+}
 
 
 
+function sendToRoomIncludingMe(socket, msgtype, msg) {
+  if (userinfo[socket.id] && userinfo[socket.id].roomname) {
+    let room = userinfo[socket.id].roomname;
+    io.to(room).emit(msgtype, msg);
+  }
+}
 
-if (require.main === module) {
-  
-  const app = express();
-  app.use(cors());
-  app.use(express.json({limit: '25mb'}));
-  app.use(express.urlencoded({ limit: '25mb',extended: false }));
-  app.use(express.static(path.join(__dirname, '../dev/public')));
 
-  var server;
-  server = http.createServer(app);  
-  let io =  this.start(server, {allowUserRooms: true});
-  
-  server.listen(3000);
+function sendToRoom(socket, msgtype, msg) {
+  if (userinfo[socket.id] && userinfo[socket.id].roomname) {
+    let room = userinfo[socket.id].roomname;
+    socket.to(room).emit(msgtype, msg);
+  }
+}
 
+
+function sendInitialState(socket, roomname) {
+  let userset = io.sockets.adapter.rooms.get(roomname);
+
+  if (userset != undefined) {
+    for (let item of userset) {
+      if (item != socket.id) {
+        {
+          sendToUser(item, "sendInitialState", socket.id);
+          break;
+        }
+      }
+    }
+  }
 }
