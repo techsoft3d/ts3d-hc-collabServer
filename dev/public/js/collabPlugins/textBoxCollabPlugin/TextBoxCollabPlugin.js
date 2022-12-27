8cbee930f56199c85648fc1bd06401d34293c522
Guido <eric5544@gmail.com>
2022-12-27 07:31:33 -0800

    textbox updated

--- dev/public/js/collabPlugins/textBoxCollabPlugin/TextBoxCollabPlugin.js
+++ dev/public/js/collabPlugins/textBoxCollabPlugin/TextBoxCollabPlugin.js
@@ -37,9 +37,9 @@ class TextBoxCollabPlugin {
         html += '<div style="white-space: nowrap;overflow:hidden;pointer-events:none;max-width:300px;position:absolute;left:0px;top:-18px;min-width:50px;width:inherit;height:15px;';
         html += 'outline-width:inherit;outline-style:solid;background-color:white;background: white;font-size:12px;font-weight:bold"><div style="overflow:hidden;width:calc(100% - 38px)">' + text + '</div>';
         html += '<div title = "Delete" style="pointer-events:all;position:absolute;right:0px;top:0px;width:10px;font-size:10px;outline-style:solid;outline-width:1px;padding-left:1px;height:inherit;cursor:pointer">&#x2715</div>';
-        html += '<div title = "Unpin" style="pointer-events:all;position:absolute;right:13px;top:0px;width:10px;font-size:10px;outline-style:solid;outline-width:1px;padding-left:1px;height:inherit;cursor:pointer"><span style="pointer-events:none;height:7px;width:7px;top:4px;left:2px;position:absolute;outline-color:black;outline-style:solid;outline-width:1px;border-radius:50%;display:inline-block;background-color:black"></span></div>';
-        html += '<div  title = "Activate Visiblity Test" title = "Unpin" style="opacity:0.3;pointer-events:all;position:absolute;right:26px;top:0px;width:10px;font-size:10px;outline-style:solid;outline-width:1px;padding-left:1px;height:inherit;cursor:pointer">';
-        html += '<div style="pointer-events:none;position:absolute;width:3px;height:3px;border:solid 1px #000;border-radius:50%;left:3px;top:5px;"></div><div style="pointer-events:none;position:absolute;width:7px;height:7px;border:solid 1px #000;border-radius:75% 15%;transform:rotate(45deg);left:1px;top:3px"></div></div>';
+        html += '<div title = "Unpin" style="pointer-events:all;position:absolute;right:12px;top:0px;width:10px;font-size:10px;outline-style:solid;outline-width:1px;padding-left:1px;height:inherit;cursor:pointer"><span style="pointer-events:none;height:7px;width:7px;top:4px;left:2px;position:absolute;outline-color:black;outline-style:solid;outline-width:1px;border-radius:50%;display:inline-block;background-color:black"></span></div>';
+        html += '<div  title = "Activate Visiblity Test" title = "Unpin" style="pointer-events:all;position:absolute;right:24px;top:0px;width:10px;font-size:10px;outline-style:solid;outline-width:1px;padding-left:1px;height:inherit;cursor:pointer">';
+        html += '<div style="opacity:0.3;pointer-events:none;position:absolute;width:3px;height:3px;border:solid 1px #000;border-radius:50%;left:3px;top:5px;"></div><div style="opacity:0.3;pointer-events:none;position:absolute;width:7px;height:7px;border:solid 1px #000;border-radius:75% 15%;transform:rotate(45deg);left:1px;top:3px"></div></div>';
         html += '</div>';
 
         let _this = this;
@@ -66,6 +66,19 @@ class TextBoxCollabPlugin {
             markup.setCheckVisibility(!markup.getCheckVisibility());
             _this.updateVisiblityTest(markup);
         });
+
+
+        $(test2).hover(
+            function() { $( this).css("background-color", "lightgrey" ); }, function() { $( this).css("background-color", "white" );}
+        );
+        $(test3).hover(
+            function() { $( this).css("background-color", "lightgrey" ); }, function() { $( this).css("background-color", "white" );}
+        );
+        $(test4).hover(
+            function() { $( this).css("background-color", "lightgrey" ); }, function() { $( this).css("background-color", "white" );}
+        );
+
+
         return test;
     }
 
@@ -90,7 +103,7 @@ class TextBoxCollabPlugin {
         else {
             if (this._collab) {
                 let user = this._collab.getLocalUser();
-                let extradiv = this.createExtraDiv(user.name + " (You)");
+                let extradiv = this.createExtraDiv(user.name);
                 let backgroundColor = new Communicator.Color(user.color[0], user.color[1], user.color[2]);
                 let markup = new hcTextBox.TextBoxMarkupItem(manager, pos, undefined, undefined, undefined, undefined, backgroundColor,
                     undefined, undefined, undefined, true, extradiv, undefined, { username: user.name, userid: user.id },false);
@@ -126,11 +139,13 @@ class TextBoxCollabPlugin {
 
         let pinnedPart = $(markup.getExtraDiv()).children()[3];
         if (markup.getCheckVisibility()) {
-            $(pinnedPart).css("opacity", "1.0");
+            $($(pinnedPart).children()[0]).css("opacity", "1.0");
+            $($(pinnedPart).children()[1]).css("opacity", "1.0");
             $(pinnedPart).prop('title', 'Deactivate Visiblity Test');
         }
         else {
-            $(pinnedPart).css("opacity", "0.3");
+            $($(pinnedPart).children()[0]).css("opacity", "0.3");
+            $($(pinnedPart).children()[1]).css("opacity", "0.3");
             $(pinnedPart).prop('title', 'Activate Visiblity Test');
         }
     }
